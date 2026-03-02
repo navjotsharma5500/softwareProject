6444d240e507834ff4713ab0a51a6783a9379efc
SuryaKantTiwari <jamcocobutter@gmail.com>
2026-03-02 18:08:34 +0530

    missing import

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import LoadingSpinner from '../components/LoadingSpinner';
 import FilterPanel from '../components/home/FilterPanel';
 import ItemsView from '../components/home/ItemsView';
 import EmptyState from '../components/EmptyState';
+import { CATEGORIES } from '../utils/constants';
 
 const Home = () => {
   const navigate = useNavigate();
